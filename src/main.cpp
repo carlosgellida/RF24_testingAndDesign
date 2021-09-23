3e88f657efdbe5ad324579f3ce70cf9d11898b44
gellidao <gellidao@gmail.com>
2021-09-23 12:29:46 -0500

    new libraries gives better performance

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,30 @@
 #include <BasicLinearAlgebra.h>
 using namespace BLA;
-#include <RadioPayload.h>
+#include <RadioPayload2.h>
 
 Matrix<4, 1> qDeseado = {1.0, 1.0, 1.0, 1.0}; 
 
 Matrix<4, 1> qCurrent = {1.0, 1.0, 1.0, 1.0}; 
+
+unsigned long timeLast ; 
 
 void setup() {
 
   Serial.begin(115200);
   
   InitialiceRadio() ;
+
+  timeLast = micros() ; 
 } // setup
 
 void loop() {
 
-  sended = send(qCurrent) ; 
+  if(micros() - timeLast > 8000){
+    timeLast = micros(); 
+    sended = send(qCurrent) ; 
   
-  recieved = recieve(qDeseado);  
+    recieved = recieve(qDeseado);  
+
+  } //if time
 
 } // loop
